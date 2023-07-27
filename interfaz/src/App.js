21c0c50df252dc57c814d5b84c10394d65cb7f60
jujgomezru <jujgomezru@unal.edu.co>
2023-07-26 19:34:14 -0500

    Github pages initial commit

---
+++ interfaz/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { CodeFrame } from './CodeFrame';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="code-frame-container"> 
+          <CodeFrame /> 
+        </div>
+      </header>
+    </div>
+  );
+};
+
+
+export default App;
